743028659e9c26c00853d30e536e0b8dc236bf66
soulkirs <69516541+soulkirs-main@users.noreply.github.com>
2020-08-14 20:36:04 +0900

    Update notice.js

--- commands/Developer Commands/notice.js
+++ commands/Developer Commands/notice.js
@@ -8,6 +8,7 @@ module.exports = {
         client.channels.cache.map(cn => {
             if(cn.name.includes("공지")){
                 cn.send(message.data.args)
+                message.channel.send("```1개의 서버에 공지가 전송되었습니다.```")
             }
         })
     }
